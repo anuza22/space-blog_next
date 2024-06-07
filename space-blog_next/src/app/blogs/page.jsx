fed641dcde6fedd2f8a868d8b9dce46327fdc457
Anuza <a_akturina@kbtu.kz>
2024-06-07 16:33:53 +0600

    add:blogs/ route

--- space-blog_next/src/app/blogs/page.jsx
+++ space-blog_next/src/app/blogs/page.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import BlogCard from "../components/blogCard";
 import data from "../data"
+import Link from 'next/link';
+
+
 export default function Blogs() {
   return (
     <div className="min-h-screen p-8 bg-space-900">
       <h1 className="text-4xl font-bold mb-8">Blogs</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {data.map((blog) => (
-               <Link key={blog.id} href={`/blogs/${blog.id}`}>
-               <a>
-                 <BlogCard title={blog.title} />
+        {data.map((blog) => (
+          <Link key={blog.id} href={`/blogs/${blog.id}`} legacyBehavior>
+            <a>
+              <BlogCard blog={blog} />
                </a>
              </Link>
             ))}
--- space-blog_next/src/app/components/blogCard.jsx
+++ space-blog_next/src/app/components/blogCard.jsx
@@ -1,7 +1,12 @@
 import Link from 'next/link'; 
 import DetailBlogCard from './detailBlogCard';
  
-const BlogCard = ({ blog }) => { 
+const BlogCard = ({blog}) => { 
+
+  if (!blog) {
+    // Render a loading state or a placeholder
+    return <div>Loading...</div>;
+  }
   return ( 
     <div className="bg-space-800 p-4 rounded-lg shadow-lg"> 
       <img src={blog.image} alt={blog.title} className="rounded-t-lg w-full h-48 object-cover" /> 
